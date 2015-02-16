273acd2d9c01d97e52a2a8add977340fec2ce697
cycorld <choi@likelion.net>
2015-02-16 19:28:35 +0900

    로그인 삽질

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,5 @@
+var serverhost = "http://192.168.0.2:3000";
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -39,7 +41,7 @@ function getImage() {
   },{
     quality: 50,
     destinationType: navigator.camera.DestinationType.FILE_URI,
-    sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+    sourceType: navigator.camera.PictureSourceType.CAMERA
   }
  );
 }
@@ -51,24 +53,89 @@ function uploadPhoto(imageURI) {
     options.mimeType="image/jpeg";
 
     var params = new Object();
-    params.value1 = "test";
-    params.value2 = "param";
+    params.klass_id = "1";
+    params.email = window.localStorage.getItem("user_email");
+    params.token = window.localStorage.getItem("user_token");
 
     options.params = params;
     options.chunkedMode = false;
 
     var ft = new FileTransfer();
-    ft.upload(imageURI, "http://yourdomain.com/upload.php", win, fail, options);
+    ft.upload(imageURI, serverhost + "/api/upload", win, fail, options);
+}
+
+function login(email, pw) {
+  var xmlhttp;
+  if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+    }
+  else
+    {// code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+  xmlhttp.onreadystatechange=function()
+    {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200)
+      {
+        //document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+        a = JSON.parse(xmlhttp.responseText);
+        window.localStorage.setItem("user_token", a["result"]);
+        window.localStorage.setItem("user_email", email);
+        console.log("===== Sign up ====");
+        console.log("CODE : " + a["code"]);
+        console.log("RESULT : " + a["result"]);
+        console.log("Saved Token : " + window.localStorage.getItem("user_token"));
+        window.location.replace("index.html");
+      }
+  }
+  xmlhttp.open("POST", serverhost + "/api/signup", true);
+  xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+  xmlhttp.send("email="+email+"&password="+pw);
+}
+
+function callList() {
+  var xmlhttp;
+  if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+    }
+  else
+    {// code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+  xmlhttp.onreadystatechange=function()
+    {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200)
+      {
+        //document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+        a = JSON.parse(xmlhttp.responseText);
+        console.log("===== Call List ====");
+        console.log("CODE : " + a["code"]);
+        console.log("RESULT : " + a["result"]);
+        document.getElementById("class_list").innerHTML = a["result"]
+      }
+  }
+  email = window.localStorage.getItem("user_email");
+  token = window.localStorage.getItem("user_token");
+  xmlhttp.open("POST", serverhost + "/api/list", true);
+  xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+  xmlhttp.send("email="+email+"&token="+token);
 }
 
 function win(r) {
     console.log("Code = " + r.responseCode);
     console.log("Response = " + r.response);
     console.log("Sent = " + r.bytesSent);
-    alert(r.response);
+    alert("업로드 하였습니다");
 }
 
 function fail(error) {
-    alert("An error has occurred: Code = " = error.code);
+    alert("업로드 실패 : Code = " = error.code);
+}
+
+function logout() {
+  window.localStorage.clear();
+  window.location.href = "login.html";
 }
 
